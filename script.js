eb071badc46772bf70cce497f401b597fa605cc4
Andre Rocha <andre.felipedsr@gmail.com>
2024-02-06 14:39:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 async function poke() {
-    const url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
-    const response = await fetch(url);
-    const pokemons = await response.json();
-    console.log(pokemons);
-    console.log(pokemons.name);
-    console.log(pokemons.game_indices[0].game_index);
+    const apikey = 'fa0345e1513193b62815809a6ec68648'
+    const cidade = 'sao paulo';
+    const SysUnit = 'metric';
+    const urlReq = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apikey}&units=${SysUnit}&lang=pt_br`;
+    //const urlReq = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apikey}`;
+    const response = await fetch(urlReq);
+    const clima = await response.json();
+    console.log(clima);
+    const main = clima.main;
+    console.log(`temperatura: ${main.temp}° Ceçsius`)
+    console.log(`sensação térmica: ${main.feels_like}° Celsius`)
+    console.log(`humidade: ${main.humidity}%`)
+    console.log(`pressao: ${main.pressure} hPa`)
+    console.log(`temperatura máxima: ${main.temp_max}° Celsius`)
+    console.log(`temperatura minima: ${main.temp_min}° Celsius`)
+    console.log(`Cidade: ${clima.name+ '. País: '+ clima.sys.country}`)
+    console.log(`main: ${clima.weather[0].main}`)
+    console.log(`Descrição: ${clima.weather[0].description}`)
+    const visibilidade = clima.visibility / 1000
+    console.log(`visibilidade: ${visibilidade} KM de distância.`)
     //document.getElementById('saida').innerHTML =
 
 }
